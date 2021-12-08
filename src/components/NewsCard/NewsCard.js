c295e917af4c491471e35434ad57c411143547bf
Theophilus Boakye Yiadom <theophilus.yiadom@amalitech.org>
2021-12-07 16:43:26 -0800

    first commit

---
+++ src/components/NewsCard/NewsCard.js
@@ -0,0 +1,86 @@
+import React, { useState, useEffect, createRef } from "react";
+import classNames from "classnames";
+import {
+	Card,
+	CardActions,
+	CardActionArea,
+	CardContent,
+	CardMedia,
+	Button,
+	Typography,
+} from "@material-ui/core";
+import useStyles from "./styles";
+const NewsCard = ({
+	article: { description, publishedAt, source, title, url, urlToImage },
+	i,
+	activeArticle,
+}) => {
+	const classes = useStyles();
+	const [elRefs, setElRefs] = useState([]);
+	const scrollToRef = (ref) => window.scroll(0, ref.current.offsetTop - 50);
+
+	useEffect(() => {
+		window.scroll(0, 0);
+
+		setElRefs((refs) =>
+			Array(20)
+				.fill()
+				.map((_, j) => refs[j] || createRef())
+		);
+	}, []);
+
+	useEffect(() => {
+		if (i === activeArticle && elRefs[activeArticle]) {
+			scrollToRef(elRefs[activeArticle]);
+		}
+	}, [i, activeArticle, elRefs]);
+
+	return (
+		<Card
+			ref={elRefs[i]}
+			className={classNames(
+				classes.card,
+				activeArticle === i ? classes.activeCard : null
+			)}
+		>
+			<CardActionArea href={url} target="_blank">
+				<CardMedia
+					className={classes.media}
+					image={
+						urlToImage ||
+						"https://media.istockphoto.com/photos/breaking-news-world-news-with-map-backgorund-picture-id1182477852?k=20&m=1182477852&s=612x612&w=0&h=I3wdSzT_5h1y9dHq_YpZ9AqdIKg8epthr8Guva8FkPA="
+					}
+				/>
+				<div className={classes.details}>
+					<Typography variant="body2" color="textSecondary" component="h2">
+						{new Date(publishedAt).toDateString()}
+					</Typography>
+					<Typography variant="body2" color="textSecondary" component="h2">
+						{source.name}
+					</Typography>
+				</div>
+
+				<Typography gutterBottom variant="h5" className={classes.title}>
+					{title}
+				</Typography>
+
+				<CardContent>
+					<Typography variant="body2" color="textSecondary" component="p">
+						{description}
+					</Typography>
+				</CardContent>
+			</CardActionArea>
+
+			<CardActions className={classes.cardActions}>
+				<Button size="small" color="primary">
+					Learn more
+				</Button>
+				<Typography variant="h5" color="textSecondary">
+					{i + 1}
+				</Typography>
+			</CardActions>
+		</Card>
+	);
+};
+
+export default NewsCard;
---
+++ src/styles.js
@@ -0,0 +1,94 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export default makeStyles((theme) => ({
+	footer: {
+		textAlign: "center",
+		position: "fixed",
+		left: 0,
+		top: "20%",
+		zIndex: 2,
+		[theme.breakpoints.down("sm")]: {
+			display: "none",
+		},
+	},
+
+	socialApps: {
+		display: "flex",
+		flexDirection: "column",
+		alignItems: "center",
+		justifyContent: "space-between",
+		height: "250px",
+		marginLeft: "5px",
+		// background: "#10043A",
+		padding: "10px",
+	},
+	socialIcons: {
+		width: "50px",
+		height: "50px",
+		borderRadius: "50px",
+		// background: "#fff",
+		display: "flex",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+
+	icons: {
+		width: "30px",
+		height: "30px",
+	},
+
+	link: {
+		textDecoration: "none",
+		color: "rgba(21, 101, 192)",
+	},
+	image: {
+		marginLeft: 20,
+	},
+	card: {
+		display: "flex",
+		justifyContent: "center",
+		alignItems: "center",
+		width: "50%",
+		padding: "3%",
+		borderRadius: 10,
+		color: "white",
+		backgroundColor: "rgba(21, 101, 192)",
+		margin: "0 12px",
+		textAlign: "center",
+		height: "25vmin",
+		[theme.breakpoints.down("sm")]: {
+			flexDirection: "column-reverse",
+			textAlign: "center",
+			width: "100%",
+			height: "initial",
+			"&:nth-of-type(1)": {
+				marginBottom: "12px",
+			},
+		},
+	},
+	infoContainer: {
+		display: "flex",
+		alignItems: "center",
+		justifyContent: "space-around",
+		[theme.breakpoints.down("sm")]: {
+			flexDirection: "column",
+		},
+	},
+	logoContainer: {
+		padding: "0 100px",
+		textAlign: "center",
+		[theme.breakpoints.down("sm")]: {
+			padding: "10px 30px",
+			fontSize: "10px",
+		},
+	},
+	alanLogo: {
+		height: "27vmin",
+		borderRadius: "15%",
+		padding: "0 5%",
+		margin: "3% 0",
+		[theme.breakpoints.down("sm")]: {
+			height: "35vmin",
+		},
+	},
+}));
